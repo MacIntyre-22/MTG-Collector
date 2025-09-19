ba74599ce7c53d4132315c8713728294d189f055
Ben MacIntyre <W0840839@myscc.ca>
2025-09-19 10:42:28 -0400

    Initial Commit

---
+++ MTG Collector/MTG_CollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTG_CollectorApp.swift
+//  MTG Collector
+//
+//  Created by Ben MacIntyre (School) on 2025-09-19.
+//
+
+import SwiftUI
+
+@main
+struct MTG_CollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
